3bc63a97c8c82608acad472317884a0a999c5d80
Pablo Oliver <olivercortespablo@gmail.com>
2022-06-01 00:41:27 +0200

    Test OSCAR splitted output.

--- oscar/root-services/tester/root-map.sh
+++ oscar/root-services/tester/root-map.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 echo "SCRIPT: Invoked function"
-echo "$INPUT_TYPE" # Esta variable ya no existe.
 echo "$INPUT_FILE_PATH"
 
 #. /root_install/bin/thisroot.sh
-echo $(whoami)
+echo "$(whoami)"
 
-echo $(cat /oscar/config/function_config.yaml)
+echo "$(cat /oscar/config/function_config.yaml)"
 
-token=echo $(grep token /oscar/config/function_config.yaml | awk '{print $2}')
-access_key=echo $(grep access_key /oscar/config/function_config.yaml | awk '{print $2}')
-secret_key=echo $(grep secret_key /oscar/config/function_config.yaml | awk '{print $2}')
+token=$(grep token /oscar/config/function_config.yaml | awk '{print $2}')
+access_key=$(grep access_key /oscar/config/function_config.yaml | awk '{print $2}')
+secret_key=$(grep secret_key /oscar/config/function_config.yaml | awk '{print $2}')
 
+mkdir "$TMP_OUTPUT_DIR/logs"
+echo "Test 1" > "$TMP_OUTPUT_DIR/logs/test1.txt"
+mkdir "$TMP_OUTPUT_DIR/out"
+echo "Test 2" > "$TMP_OUTPUT_DIR/out/test2.txt"
 
-echo $token
-echo $access_key
-echo $secret_key
+echo "$token"
+echo "$access_key"
+echo "$secret_key"
+
+env
