95203d320edf3a38254a29d8163d2c977f0e4757
Pablo Oliver <olivercortespablo@gmail.com>
2022-06-14 10:05:39 +0200

    Fix net_io_counters benchmark

--- oscar/root-services/root-mapper-bench/bench.py
+++ oscar/root-services/root-mapper-bench/bench.py
@@ -13,6 +13,7 @@ p_attrs = ['create_time', 'cpu_times', 'io_counters',
            'num_ctx_switches', 'memory_full_info']
 
 bench_start = p.as_dict(attrs=p_attrs)
+net_start = psutil.net_io_counters()
 
 # Call to actual process.
 mapper_p = psutil.Popen(mapper_args)
@@ -27,7 +28,7 @@ while mapper_p.poll() is None:
     sleep(0.5)
 
 bench_end = p.as_dict(attrs=p_attrs[1:])
-
+net_end = psutil.net_io_counters()
 
 #Write benchmarks to files.
 file_name = sys.argv[1].split('/')[-1]
@@ -36,9 +37,17 @@ ts = uuid.uuid1()
 f_bs = open(f'{sys.argv[2]}/benchmarks/mapper_{file_name}_start_{ts}', 'wb')
 f_be = open(f'{sys.argv[2]}/benchmarks/mapper_{file_name}_end_{ts}', 'wb')
 f_cp = open(f'{sys.argv[2]}/benchmarks/mapper_{file_name}_cpupercent_{ts}', 'wb')
+f_ns = open(f'{sys.argv[2]}/benchmarks/mapper_{file_name}_netiost_{ts}', 'wb')
+f_ne = open(f'{sys.argv[2]}/benchmarks/mapper_{file_name}_netioend_{ts}', 'wb')
+
 f_bs.write(cloudpickle.dumps(bench_start))
 f_be.write(cloudpickle.dumps(bench_end))
 f_cp.write(cloudpickle.dumps(cpu_usage))
+f_ns.write(cloudpickle.dumps(net_start))
+f_ne.write(cloudpickle.dumps(net_end))
+
 f_bs.close()
 f_be.close()
 f_cp.close()
+f_ns.close()
+f_ne.close()
